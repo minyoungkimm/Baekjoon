e4ba0ee34616716a666b43732344f094feabb30c
KBet <sgm02101@hanmail.net>
2021-02-25 13:55:47 +0900

    study

--- 동적계획법.py
+++ 동적계획법.py
@@ -1,17 +1,17 @@
 # 피보나치 함수
-# T = int(input())
-#
-# for _ in range(T):
-#     n = int(input())
-#     zero = [1,0,1]
-#     one=[0,1,1]
-#     if n >= 3:
-#         for i in range(3, n+1):
-#             zero.append(zero[i-2] + zero[i-1])
-#             one.append(one[i-2] + one[i-1])
-#     print(zero[n],one[n])
+T = int(input())
 
-#
+for _ in range(T):
+    n = int(input())
+    zero = [1,0,1]
+    one=[0,1,1]
+    if n >= 3:
+        for i in range(3, n+1):
+            zero.append(zero[i-2] + zero[i-1])
+            one.append(one[i-2] + one[i-1])
+    print(zero[n],one[n])
+
+
 
 # 신나는 함수실행
 memo = [[[0]*21 for _ in range(21)] for _ in range(21)]
